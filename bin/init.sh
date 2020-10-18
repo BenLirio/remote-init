e470c7642c1de01d553c62fb621fe57f8f4a6bee
Ben Lirio <benjlirio@gmail.com>
2020-10-18 01:40:38 +0000

    add go install

---
+++ bin/init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Make sure user is root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# install dependencies
+apt-get update -y
+apt-get install git -y
+apt-get install wget -y
+
+# Install compilers
+go_version=go1.15.2.linux-amd64.tar.gz
+wget -c https://golang.org/dl/$go_version
+shasum -a 256 $go_version
+tar -C /usr/local -svzf $go_version
+export PATH=$PATH:/usr/local/go/bin
